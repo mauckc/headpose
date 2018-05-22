7fa34a3f32dec2029e6b50e56563e1502a666d2a
Ross Mauck <ross@rossmauck.com>
2018-05-22 16:11:57 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python python/save-video-pose.py -p models/shape_predictor_68_face_landmarks.dat -v media/sample.mp4
+python python/save-video-pose.py -p models/shape_predictor_68_face_landmarks.dat -v media/obama.mp4
