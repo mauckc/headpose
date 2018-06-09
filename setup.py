98e899c3b61363d42092e0a8fc7fc46c7f769af9
Ross Mauck <ross@rossmauck.com>
2018-06-09 15:07:06 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+wget https://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
 bzip2 -vd shape_predictor_68_face_landmarks.dat.bz2
 mkdir -vp models
 mv -v shape_predictor_68_face_landmarks.dat models
